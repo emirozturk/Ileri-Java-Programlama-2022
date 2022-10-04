4985a5ef4279cc18a33d3504161126ee18d9fd67
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-10-04 17:37:30 +0300

    file path converted to generic

--- Uygulama3/src/main/java/org/emirozturk/FileOps.java
+++ Uygulama3/src/main/java/org/emirozturk/FileOps.java
@@ -3,23 +3,32 @@ package org.emirozturk;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.nio.file.Paths;
 
 public class FileOps {
-    public static Squad readFile(String filepath){
+    public static Squad readFile(String filepath) {
         try {
             var mapper = new ObjectMapper();
-            return mapper.readValue(new File(filepath), Squad.class);
-        }catch (Exception ex){
+
+            return mapper.readValue(new File(getFilePath(filepath)), Squad.class);
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
             return null;
         }
     }
-    public static void writeFile(Squad squad,String filepath){
-        try{
+
+    public static void writeFile(Squad squad, String filepath) {
+        try {
             var mapper = new ObjectMapper();
-            mapper.writeValue(new File(filepath),squad);
-        }catch (Exception ex){
+            mapper.writeValue(new File(getFilePath(filepath)), squad);
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
+    }
+
+    public static String getFilePath(String filename) {
+        File currentDirFile = new File(".");
+        String helper = currentDirFile.getAbsolutePath();
+        return Paths.get(helper.substring(0, helper.length() - 1), "Uygulama3", filename).toString();
     }
 }
