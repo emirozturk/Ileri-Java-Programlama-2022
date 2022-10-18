89e233de56d657e76435ce76251ba958f180d877
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-10-18 13:36:02 +0300

    convertToFloat method implemented

--- Uygulama5/src/main/java/com/emirozturk/uygulama4/Model/EQ.java
+++ Uygulama5/src/main/java/com/emirozturk/uygulama4/Model/EQ.java
@@ -20,22 +20,32 @@ public class EQ {
         this.cozumNiteligi = cozumNiteligi;
     }
 
-    public static EQ parseEt(List<String> liste){
-        var tarihSaat = LocalDateTime.parse(liste.get(0)+" "+liste.get(1), DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
+    public static EQ parseEt(List<String> liste) {
+        var tarihSaat = LocalDateTime.parse(liste.get(0) + " " + liste.get(1), DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
         var koordinat = new Koordinat(Float.parseFloat(liste.get(2)),
                 Float.parseFloat(liste.get(3)),
                 Float.parseFloat(liste.get(4)));
-        var md = liste.get(5).equals("-.-") ?0:Float.parseFloat(liste.get(5));
-        var ml = liste.get(6).equals("-.-") ?0:Float.parseFloat(liste.get(6));
-        var mw = liste.get(7).equals("-.-") ?0:Float.parseFloat(liste.get(7));
-        var magnitude = new Magnitude(md,ml,mw);
-        String yer="";
-        for(int i=8;i<liste.size()-1;i++){
-            yer += liste.get(i)+ " ";
+        var md = liste.get(5).equals("-.-") ? 0 : Float.parseFloat(liste.get(5)); // alternatif bir kullanim
+        var ml = convertToFloat(liste.get(6)); // method kullanarak kendimiz cevirdik
+        var mw = convertToFloat(liste.get(7));
+        var magnitude = new Magnitude(md, ml, mw);
+        String yer = "";
+        for (int i = 8; i < liste.size() - 1; i++) {
+            yer += liste.get(i) + " ";
         }
-        String nitelik = liste.get(liste.size()-1);
-        return new EQ(tarihSaat,koordinat,magnitude,yer,nitelik);
+        String nitelik = liste.get(liste.size() - 1);
+        return new EQ(tarihSaat, koordinat, magnitude, yer, nitelik);
     }
+
+    public static Float convertToFloat(String value) {
+        try {
+            return Float.parseFloat(value);
+        } catch (Exception e) {
+            //cevirirken bir hata yakalarsa 0 degeri dondursun
+            return 0F; // 0F -> 0 float demek
+        }
+    }
+
     public LocalDateTime getTarihSaat() {
         return tarihSaat;
     }
@@ -75,8 +85,9 @@ public class EQ {
     public void setCozumNiteligi(String cozumNiteligi) {
         this.cozumNiteligi = cozumNiteligi;
     }
+
     @Override
-    public String toString(){
+    public String toString() {
         return "%s %s %s %s %s".formatted(
                 tarihSaat.toString(),
                 koordinat,
