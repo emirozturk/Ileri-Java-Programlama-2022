a48b7bcdc0abfcb1fa3774d1a25eeaa0c99ec185
Emir Öztürk <emirozturk3@gmail.com>
2022-11-22 11:57:27 +0300

    Uygulama8

---
+++ Uygulama8/BenzerBul/src/main/java/com/emirozturk/benzerbul/BenzerBulApplication.java
@@ -0,0 +1,13 @@
+package com.emirozturk.benzerbul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenzerBulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BenzerBulApplication.class, args);
+    }
+
+}
